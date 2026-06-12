bc367e9154ae575296a0276e5191b4f18d2a99d4
ezekielelvis <ezeamartey777@gmail.com>
2026-06-12 01:31:35 +0000

    refactor: simplify NotFound component by removing unnecessary elements and improving structure

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,30 +1,12 @@
 import { Button } from "@/components/Button";
 import Link from "next/link";
 import { siteConfig } from "./siteConfig";
-
-import { SolarLogo } from "../../public/SolarLogo";
-import NotFoundAnimation from '@/images/NotFoundAnimation';
+import NotFoundAnimation from "@/images/NotFoundAnimation";
 
 export default function NotFound() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <Link href={siteConfig.baseLinks.home}>
-        <SolarLogo className="mt-6 h-10" />
-      </Link>
-
-      <div className="mt-6">
-        <NotFoundAnimation width={300} height={300} />
-      </div>
-
-      <p className="mt-6 text-4xl font-semibold text-amber-600 sm:text-5xl">
-        404
-      </p>
-      <h1 className="mt-4 text-2xl font-semibold text-gray-900">
-        Page not found
-      </h1>
-      <p className="mt-2 text-sm text-gray-600">
-        This page doesn’t exist yet. Check the home page or try another link.
-      </p>
+      <NotFoundAnimation width={300} height={300} />
       <Button asChild className="group mt-8" variant="light">
         <Link href={siteConfig.baseLinks.home}>Go to the home page</Link>
       </Button>
